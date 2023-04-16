c554535bcbbeb003fa9cdc65eecb98cc6f19b4b7
perfectearth <mistudebbp@gmail.com>
2023-04-16 20:46:21 +0600

    Initial commit

--- app/src/main/java/com/perfectearth/bhagavadgita/UserRegister.java
+++ app/src/main/java/com/perfectearth/bhagavadgita/UserRegister.java
@@ -189,7 +189,6 @@ public class UserRegister extends AppCompatActivity {
             inTextPassword.setError("Please fill");
             inTextPassword.requestFocus();
         }
-        inTextPhone.getText().clear();
         inTextPassword.getText().clear();
         CustomProgress.showProgressBar(this,false,"Please\nWait....");
         loginUser(phone, password);
