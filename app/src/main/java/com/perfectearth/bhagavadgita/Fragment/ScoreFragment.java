475af4ea5036ee421eef26a27192f028b58c23f3
perfectearth <mistudebbp@gmail.com>
2023-04-17 02:41:50 +0600

    Initial commit

---
+++ app/src/main/java/com/perfectearth/bhagavadgita/Fragment/ScoreFragment.java
@@ -0,0 +1,24 @@
+package com.perfectearth.bhagavadgita.Fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.perfectearth.bhagavadgita.R;
+public class ScoreFragment extends Fragment {
+
+    public ScoreFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_score, container, false);
+    }
+}
